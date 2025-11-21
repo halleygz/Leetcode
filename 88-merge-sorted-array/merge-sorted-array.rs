d99311ce30ed9c3bf5f3027b7dd99467c014a0da
Halleluya Mulugeta <98106633+halleygz@users.noreply.github.com>
2025-11-21 09:54:03 +0300

    Time: 0 ms (100.00%) | Memory: 2.1 MB (99.77%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        let (mut m, mut n) = (m as usize, n as usize);
+        
+        while n > 0 {
+            if m > 0 && nums1[m - 1] > nums2[n - 1] {
+                nums1[m + n - 1] = nums1[m - 1];
+                m -= 1;
+            } else {
+                nums1[m + n - 1] = nums2[n - 1];
+                n -= 1;
+            }
+        }
+    }
+}
