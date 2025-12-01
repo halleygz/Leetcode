bc352b752b36e3da4bc19fe3bfb7024355c6703e
Halleluya Mulugeta <98106633+halleygz@users.noreply.github.com>
2025-12-01 17:41:20 +0300

    Time: 7 ms (43.09%) | Memory: 19 MB (31.21%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,11 +4,18 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # find first zero(valid position) i
+        # left = 0
+        # for right in range(len(nums)):
+        #     if nums[right] != 0:
+        #         nums[right], nums[left] = nums[left], nums[right]
+        #         left += 1
+        # return nums
         left = 0
-        for right in range(len(nums)):
+        right = 0
+        while right <  len(nums):
             if nums[right] != 0:
                 nums[right], nums[left] = nums[left], nums[right]
-                left += 1
-        return nums
-
+                left+=1
+            right+=1
+    
             
