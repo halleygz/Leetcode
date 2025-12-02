72736453dfb3e09e0bbbe18d72befda6fd549a0b
Halleluya Mulugeta <98106633+halleygz@users.noreply.github.com>
2025-12-02 20:37:43 +0300

    Time: 75 ms (98.21%) | Memory: 30.9 MB (67.82%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,37 +1,26 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        n = len(matrix)
-        m = len(matrix[0])
+        R, C = len(matrix), len(matrix[0])
 
-        self.prefix = [[0]*m for _ in range(n)]
+        self.prefArr = [[0] * (C + 1) for _ in range(R + 1)]
 
-        for i in range(n):
-            for j in range(m):
-                
-                self.prefix[i][j] = matrix[i][j]
-
-                if i > 0:
-                    self.prefix[i][j] += self.prefix[i-1][j]
-                if j > 0:
-                    self.prefix[i][j] += self.prefix[i][j-1]
-                if i > 0 and j > 0:
-                    self.prefix[i][j] -= self.prefix[i-1][j-1]
+        for r in range(R):
+            prefix = 0
+            for c in range(C):
+                prefix += matrix[r][c]
+                above = self.prefArr[r][c + 1]
+                self.prefArr[r + 1][c + 1] = prefix + above
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        range = self.prefix[row2][col2]
+        r1, c1, r2, c2 = row1 + 1, col1 + 1, row2 + 1, col2 + 1
 
+        total = self.prefArr[r2][c2]
+        upper = self.prefArr[r1-1][c2]
+        left = self.prefArr[r2][c1-1]
 
-        if row1 > 0:
-            range -= self.prefix[row1-1][col2]
-        if col1 > 0:
-            range -= self.prefix[row2][col1-1]
-        if row1 > 0 and col1 > 0:
-            range += self.prefix[row1-1][col1-1]
-
-        return range
-
-        
+        area = total - upper - left + self.prefArr[r1-1][c1-1]
+        return area
 
 
 # Your NumMatrix object will be instantiated and called as such:
